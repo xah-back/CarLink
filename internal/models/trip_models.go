8ad9b5d6af59797b597a2197c796ebac24d16392
Askhab90 <askhab108@mail.ru>
2025-12-22 14:16:40 +0300

    сделал дто авто

--- internal/models/trip_models.go
+++ internal/models/trip_models.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	"github.com/mutsaevz/team-5-ambitious/internal/constants"
 )
 
 type Trip struct {
@@ -21,27 +23,11 @@ type Trip struct {
 }
 
 type TripCreateRequest struct {
-	DriverID       uint      `json:"driver_id"`
-	CarID          uint      `json:"car_id"`
-	From           string    `json:"from"`
-	To             string    `json:"to"`
-	StartTime      time.Time `json:"start_time"`
-	DurationMin    int       `json:"duration_min"`
-	TotalSeats     int       `json:"total_seats"`
-	AvailableSeats int       `json:"available_seats"`
-	Price          int       `json:"price"`
-	TripStatus     string    `json:"trip_status"`
-}
-
-type TripUpdateRequest struct {
-	DriverID       uint      `json:"driver_id"`
-	CarID          uint      `json:"car_id"`
-	From           string    `json:"from"`
-	To             string    `json:"to"`
-	StartTime      time.Time `json:"start_time"`
-	DurationMin    int       `json:"duration_min"`
-	TotalSeats     int       `json:"total_seats"`
-	AvailableSeats int       `json:"available_seats"`
-	Price          int       `json:"price"`
-	TripStatus     string    `json:"trip_status"`
+	From           string               `json:"from"`
+	To             string               `json:"to"`
+	StartTime      time.Time            `json:"start_time"`
+	DurationMin    int                  `json:"duration_min"`
+	AvailableSeats int                  `json:"available_seats"`
+	Price          int                  `json:"price"`
+	TripStatus     constants.TripStatus `json:"trip_status"`
 }
